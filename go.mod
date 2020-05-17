85ea10625b85175afdbc7d48334edcca197872fb
balus <balus@foxmail.com>
2020-05-17 22:16:14 +0800

    Add pprof support.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/BalusChen/IKHNAIE v0.0.0-20200515085340-220146868bf2
+	github.com/gin-contrib/pprof v1.3.0
 	github.com/gin-gonic/gin v1.6.2
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/hyperledger/fabric-sdk-go v1.0.0-beta1
--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/BalusChen/IKHNAIE_API/views"
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
+	pprof.Register(router)
 	views.InitRoutes(router)
 
 	panic(router.Run(":9877"))
